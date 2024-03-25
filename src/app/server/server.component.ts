c9a5546fb93fc104639ffc6c34a64b636c1aad4c
Rahul <rahulkrishnappa94@gmail.com>
2024-03-25 17:39:18 +0530

    added custom component

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  //selector where we can use the component in other component files
+  selector: 'app-server',
+  //contain the html code of the component
+  templateUrl: './server.component.html',
+})
+export class ServerComponent {}
