7b1a21bd4e811d59a92f1071964ba1de59175dcd
Rahul <rahulkrishnappa94@gmail.com>
2024-04-08 16:36:37 +0530

    String Interpolation and Property binding

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 
 @Component({
-  //selector where we can use the component in other component files
+  //declare selector where we can use the component in other component files, can also use classname, attribute
   selector: 'app-server',
-  //contain the html code of the component
+  //url for html file of the component
   templateUrl: './server.component.html',
+  //url for style of the component
+  styleUrl: './server.component.css',
 })
 export class ServerComponent {}
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -3,5 +3,22 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-servers',
   templateUrl: './servers.component.html',
+  styles: [
+    `
+      div {
+        border: 1px solid blue;
+        padding: 10px 15px;
+      }
+    `,
+  ],
 })
-export class ServersComponent {}
+export class ServersComponent {
+  serverStatus = 'online';
+  allowNewServer = true;
+  constructor() {
+    setTimeout(() => {
+      this.serverStatus = 'offline';
+      this.allowNewServer = false;
+    }, 5000);
+  }
+}
